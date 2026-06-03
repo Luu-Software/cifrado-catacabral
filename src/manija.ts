0bfc6a691986271365c38f168a666979788f6dfd
CatalinaCabral <catalinaisabelcabral@gmail.com>
2026-06-03 15:57:55 -0300

    Update manija.ts
    Manija terminado

--- src/manija.ts
+++ src/manija.ts
@@ -1,10 +1,28 @@
 import { preguntar } from './lib/consola.ts';
 
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-  return 0; // COMPLETAR
+  let longitudNumero=16;
+  let potencia= longitudNumero-tamañoPrefijo;
+  let divisor= Math.pow(10,potencia);
+  return Math.floor(numero/divisor)
 }
 
 let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+let tarjeta:number=Number(tarjetaTexto);
+
+let prefijo1=obtenerPrefijo(tarjeta,1);
+let prefijo2=obtenerPrefijo(tarjeta,2);
 
 let red: string = ''; // COMPLETAR
+if (prefijo2===34 || prefijo2===37){
+  red='AMEX';
+}
+ else if(prefijo1===4){
+  red='VISA';
+ }
+ else if(prefijo2===51 || prefijo2===52 || prefijo2===53 || prefijo2===54){
+  red='MASTERCARD';}
+  else {
+    red='DESCONOCIDA';
+  }
 console.log(red);
